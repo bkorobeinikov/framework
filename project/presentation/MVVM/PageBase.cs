e0abb830dc7e85a1ad27b2d0920050d1422eef2f
bobasoft <korobeinikov@outlook.com>
2013-01-12 18:15:20 +0200

    added presentation page

---
+++ project/presentation/MVVM/PageBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace Bobasoft.Presentation.MVVM
+{
+    public class PageBase : Page
+    {
+        //======================================================
+        #region _Public properties_
+
+        public PageModel Model
+        {
+            get { return (PageModel)DataContext; }
+            set
+            {
+                if (DataContext != value)
+                {
+                    if (value != null)
+                    {
+                        value.Page = this;
+                        value.InitializeInternal();
+                    }
+                    DataContext = value;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ project/presentation/MVVM/PageModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Bobasoft.Presentation.MVVM
+{
+    public class PageModel : ModelBase
+    {
+        //======================================================
+        #region _Public properties_
+
+        public PageBase Page { get; set; }
+
+        #endregion
+    }
+}
