93eee5a0aacf1e996dd4ac6db0f92e00c1c2559a
bobasoft <korobeinikov@outlook.com>
2013-04-01 00:18:09 +0300

    added routed activated/deactivated events

--- project/presentation/MVVM/WindowBase.cs
+++ project/presentation/MVVM/WindowBase.cs
@@ -1,9 +1,30 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Bobasoft.Presentation.MVVM
 {
     public class WindowBase : Window
     {
+        //======================================================
+        #region _Constructors_
+
+        public WindowBase()
+        {
+            base.Activated += (sender, args) =>
+                {
+                    var newEventArgs = new RoutedEventArgs(ActivatedEvent);
+                    RaiseEvent(newEventArgs);
+                };
+
+            base.Deactivated += (sender, args) =>
+                {
+                    var newEventArgs = new RoutedEventArgs(DeactivatedEvent);
+                    RaiseEvent(newEventArgs);
+                };
+        }
+
+        #endregion
+
         //======================================================
         #region _Public properties_
 
@@ -23,6 +44,33 @@ namespace Bobasoft.Presentation.MVVM
                 }
             }
         }
+
+        // Provide CLR accessors for the event 
+        public new event RoutedEventHandler Activated
+        {
+            add { AddHandler(ActivatedEvent, value); }
+            remove { RemoveHandler(ActivatedEvent, value); }
+        }
+
+        public new event RoutedEventHandler Deactivated
+        {
+            add { AddHandler(DeactivatedEvent, value); }
+            remove { RemoveHandler(DeactivatedEvent, value); }
+        }
+
+        #endregion
+
+        //======================================================
+        #region _Private, protected, internal methods_
+
+
+        #endregion
+
+        //======================================================
+        #region _Fields_
+
+        public static readonly RoutedEvent ActivatedEvent = EventManager.RegisterRoutedEvent("Activated", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(WindowBase));
+        public static readonly RoutedEvent DeactivatedEvent = EventManager.RegisterRoutedEvent("Deactivated", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(WindowBase));
 
         #endregion
     }
